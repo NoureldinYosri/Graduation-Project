0bac1fb4f0b79260c82de05a6033931c3bdeaf62
Noureldin <noureldinyosri@gmail.com>
2017-06-18 15:51:00 +0200

    updated nn_controller.py

--- modules/nn_controller.py
+++ modules/nn_controller.py
@@ -31,7 +31,6 @@ def assign_vals(labels):
 def read_data(path):
     """returns path X,Y of data where X[i] is an image and Y[i] is its label"""
     data = {}
-    val = {"no":-1,"undetermined":0,"yes":1}
     print ("started reading data")
     start_time = time.time()
     labels = []
@@ -59,26 +58,25 @@ def read_data(path):
     Y = np.array(Y)
     return X,Y
 
-def transform_data(imgs,labels,som,m,n,mylogger):
+def transform_data(imgs,labels,surf,som,m,n,mylogger):
     data = zip(imgs, labels)
     res_imgs = []
     res_labels = []
-    surf = cv2.xfeatures2d.SURF_create(10000)
     print ("started transforming data")
     cnt = 0
     start_time = time.time()
     for img, label in data:
         kp, des = surf.detectAndCompute(img,None)
-        if des is None: continue
         compressed = [0 for i in range(m*n)]
-        for feature_description in des:
-            cnt += 1
-            activation_map = som.get_surface_state(np.array(feature_description).reshape(1,64))
-            for match in som.get_bmus(activation_map):
-                compressed[match[0]*n + match[1]] += 1
+        if des is not None:
+            for feature_description in des:
+                cnt += 1
+                activation_map = som.get_surface_state(np.array(feature_description).reshape(1,64))
+                for match in som.get_bmus(activation_map):
+                    compressed[match[0]*n + match[1]] += 1
         res_imgs.append(np.array(compressed,dtype = np.float32))
         res_labels.append(label)
-
+    
     elapsed_time = time.time() - start_time
     minutes = elapsed_time/60
     seconds = elapsed_time%60
@@ -114,32 +112,49 @@ def split(X,Y,testSize = 0.3):
     Y_test = np.array(Y_test,dtype = np.float32)
     return X_train,Y_train,X_test,Y_test
 
+def create_error_matrix(Y_true,prediction):
+    vals = list(set(Y_true));
+    n = len(vals);
+    error_matrix = [[0 for i in range(n)] for j in range(n)]
+    for i in range(n):
+        for j in range(n):
+            error_matrix[i][j] = sum(np.array(Y_true==vals[i]) * np.array(prediction==vals[j]));  
+    return error_matrix;
+
+def get_most_frequent(Y):
+    freq = {};
+    max_freq = 0;
+    for x in Y:
+        if x not in freq: freq[x] = 0;
+        freq[x] += 1;
+        max_freq = max(max_freq,freq[x]);
+    for x in freq:
+        if freq[x] == max_freq:
+            return x;
+    return 0;
 
 def conduct_experiment(path,som_shape,hidden_layer_shape,surf_threshold,module_name):
     mylogger = logger.logger(utils.join_parent('logger', 2),'working on %s with MLP and SOM is %s and %s hidden layer'%(module_name,str(som_shape),str(hidden_layer_shape)))
     X,Y = read_data(path)
     surf = cv2.xfeatures2d.SURF_create(surf_threshold)
     som = get_som(path,surf,som_shape[0],som_shape[1],mylogger)
-    X,Y = transform_data(X,Y,som,som_shape[0],som_shape[1],mylogger)
+    X,Y = transform_data(X,Y,surf,som,som_shape[0],som_shape[1],mylogger)
     X_train,Y_train,X_test,Y_test = split(X,Y)
     clf = get_clf(X_train,Y_train,hidden_layer_shape,mylogger)
     train_acc = accuracy_score(Y_train, clf.predict(X_train))*100
     Y_predict = clf.predict(X_test)
     test_acc = accuracy_score(Y_test, Y_predict)*100
-    baseline_acc = sum(Y_test == 1)*100.0/len(Y_test)
-    print (Y_test[:10],Y_predict[:10])
+    baseline_acc = sum(Y_test == get_most_frequent(Y_train))*100.0/len(Y_test)
     print ("training accuracy is %.10f, test accuracy is %.10f, baseline_acc is %.10f"%(train_acc,test_acc,baseline_acc))
     plt.hist(np.array(clf.predict(X_test)))
     plt.savefig('histogram.png')
     plt.show()
-    error_matrix = [[0 for i in range(3)] for j in range(3)]
-    for i in range(3):
-        for j in range(3):
-            error_matrix[i][j] = sum(np.array(Y_test==(i-1)) * np.array(Y_predict==(j-1)))
+    error_matrix = create_error_matrix(Y_test,Y_predict);
     print (error_matrix)
     mylogger.save(clf,'MLP model training accuracy is %.10f, test accuracy is %.10f, baseline_acc is %.10f'%(train_acc,test_acc,baseline_acc) + "\n error matrix contains " + str(error_matrix) )    
 
     
 if __name__ == "__main__":
     path = utils.join_parent("goal", 2)
+    path = '/home/noureldin/Desktop/GP/dataset/BFC VS MAG'
     conduct_experiment(path,[20,20],[5,5],4000,"goal")
